b49cf9910681759cec9416d01c66656fc2332ad9
jaydeepchavda <jaydipchavda2216@gmail.com>
2026-06-13 21:20:15 +0000

    added env and docker file for server and client side to build image and run container

--- server/index.js
+++ server/index.js
@@ -1,24 +1,45 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
+dotenv.config();
+
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL =
+process.env.CONNECTION_URL;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+const PORT =
+process.env.PORT || 5000;
 
-mongoose.set('useFindAndModify', false);
+mongoose
+.connect(CONNECTION_URL,{
+useNewUrlParser:true,
+useUnifiedTopology:true
+})
+.then(() =>
+app.listen(PORT,
+() =>
+console.log(
+`Server Running on Port ${PORT}`
+)
+)
+)
+.catch((error)=>
+console.log(error)
+);
+
+mongoose.set(
+'useFindAndModify',
+false
+);
