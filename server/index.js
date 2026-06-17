7e214e5abacd1674a0c1e89a8011440aec964ee9
jaydeepchavda <jaydipchavda2216@gmail.com>
2026-06-17 17:46:47 +0000

    Readiness and Liveness Probes for health checks added in project

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ dotenv.config();
 
 const app = express();
 
+
+app.get('/health', (req, res) => {
+
+  res.status(200).json({
+
+    status: 'ok'
+
+  });
+
+});
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
@@ -17,30 +28,32 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-process.env.CONNECTION_URL;
+    process.env.CONNECTION_URL;
 
 const PORT =
-process.env.PORT || 5000;
+    process.env.PORT || 5000;
+
+
 
 mongoose
-.connect(CONNECTION_URL,{
-useNewUrlParser:true,
-useUnifiedTopology:true
-})
-.then(() =>
-app.listen(PORT,
-() =>
-console.log(
-`Server Running on Port ${PORT}`
-)
-)
-)
-.catch((error)=>
-console.log(error)
-);
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() =>
+        app.listen(PORT,
+            () =>
+                console.log(
+                    `Server Running on Port ${PORT}`
+                )
+        )
+    )
+    .catch((error) =>
+        console.log(error)
+    );
 //initial commit for pipeline testing in the github action 
 
 mongoose.set(
-'useFindAndModify',
-false
+    'useFindAndModify',
+    false
 );
